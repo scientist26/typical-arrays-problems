461726fba161aaac2353c1c836a5bf261c1c16be
Andrei Bezmen <andryshabez26@gmail.com>
2020-02-20 23:16:16 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+
+  return (array === undefined || array.length == 0) ? 0 :
+  array.sort((a, b) => a - b )[0];
+
 }
 
 exports.max = function max (array) {
-  return 0;
+
+  return (array === undefined || array.length == 0) ? 0 : 
+  array.sort((a, b) => a - b ).reverse()[0];
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+  return (array === undefined || array.length == 0) ? 0 : 
+  array.reduce((sum, current) => sum + current) / array.length
+
 }
